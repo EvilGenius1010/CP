d897f5aa4973bd48537334f83d77e297c47c95a9
quirkyNashedi <harshavardhan.mitblr2022@learner.manipal.edu>
2024-08-04 15:48:20 +0530

    added initial questions

---
+++ four.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+
+if(n==1){
+cout<<"Ehab"<<"\n";
+}
+else if(n%2==0){
+cout<<"Mahmoud"<<"\n";
+}
+else if(n%2!=0){
+cout<<"Ehab"<<"\n";
+}
+
+return 0;
+}
