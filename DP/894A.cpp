01a83c68e4c0185f83959ee8be65efb3f26c6584
quirkyNashedi <kolhatkarh@gmail.com>
2025-03-05 10:43:26 +0530

    commit before work 1

---
+++ DP/894A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+#include<map>
+#include<cmath>
+using namespace std;
+#define vi vector<int>;
+#define ll long long
+#define el "\n"
+
+void solve() {
+string s;
+cin>>s;
+
+
+
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+
+    solve();
+}
