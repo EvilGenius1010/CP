a069130c49fd9e28f5ca94a4fdcf3707630ee706
EvilGenius1010 <kolhatkarh@gmail.com>
2025-05-15 12:39:36 +0000

    Learned fibonacci

---
+++ DP/Learning/fibonacci.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int f(int n, vector<int> &dp){
+    if(n<=1) {
+        // cout<<n<<"\n";
+        dp[n]=n;
+        return n;
+    }
+    if(dp[n]!=-1) return dp[n];
+
+
+    dp[n] = f(n-1,dp)+f(n-2,dp);
+    return f(n-1,dp)+f(n-2,dp);
+}
+
+void solve(){
+    int n;
+    cin>>n;
+
+    // int dp[n+1]={-1};
+    vector<int> dp(n+1,-1);
+    cout<<f(n,dp)<<"\n";
+
+     for(unsigned int i = 0; i < dp.size(); i++)
+  {
+    cout<<"i is"<< i<<"for  "<< dp[i] << "\n";
+  }
+  cout<<endl;
+}
+
+
+int main(){
+
+    solve();
+    
+
+    
+    return 0;
+}
